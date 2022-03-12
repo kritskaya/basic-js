7a6c011d8db739f86a0339893c1af6c193717dbc
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-03-12 09:36:38 +0300

    feat: extended-repeater task done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	return new Array(options.repeatTimes || 1)
+		.fill(str)
+		.map(item => item + new Array(options.additionRepeatTimes || 1)
+										.fill(options.addition !== undefined ?  options.addition !== null ? options.addition : "null" : "")
+										.join(options.additionSeparator || "|")
+		)
+		.join(options.separator || "+");
 }
 
+console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }) );
+//'truefalse!!!false??? truefalse!!!false??? truefalse!!!false'
+
 module.exports = {
-  repeater
+	repeater
 };
