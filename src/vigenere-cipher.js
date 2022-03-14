2a799b09cc2cc85479fac3dfe348b8b47d95d03c
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2022-03-14 11:21:27 +0300

    feat: implement vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	constructor(direct = true) {
+		this.direct = direct;
+	}
+	encrypt(message, key) {
+		if (!message || !key) throw new Error("Incorrect arguments!");
+
+		const keyMessage = this.getKeyMessage(message, key);
+		
+		const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let encoded = [];
+		let notAlphabet = 0;
+
+		message.split("").forEach((char, index) => {
+			let mIndex = alphabet.indexOf(char.toUpperCase());
+			let kIndex = alphabet.indexOf(keyMessage[index - notAlphabet].toUpperCase());
+			
+			if (mIndex !== - 1) {
+				encoded.push(alphabet[(mIndex + kIndex) % 26]);
+			} else {
+				encoded.push(message[index]);
+				notAlphabet++;
+			}
+		});
+
+		return this.direct ? encoded.join("") : encoded.reverse().join("");
+	}
+	decrypt(encryptedMessage, key) {
+		if (!encryptedMessage || !key) throw new Error("Incorrect arguments!");
+
+		const keyMessage = this.getKeyMessage(encryptedMessage, key);
+
+		const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let decoded = [];
+		let notAlphabet = 0;
+
+		encryptedMessage.split("").forEach((char, index) => {
+			let eIndex = alphabet.indexOf(char.toUpperCase());
+			let kIndex = alphabet.indexOf(keyMessage[index - notAlphabet].toUpperCase());
+
+			if (eIndex !== - 1) {
+				decoded.push(alphabet[(eIndex - kIndex) % 26] || alphabet[26 + eIndex - kIndex]);
+			} else {
+				decoded.push(encryptedMessage[index]);
+				notAlphabet++;
+			}
+		});
+
+		return this.direct ? decoded.join("") : decoded.reverse().join("");
+	}
+	getKeyMessage(message, key) {
+		return key.repeat(Math.ceil(message.length / key.length))
+					 .slice(0, message.length);
+	}
 }
 
 module.exports = {
-  VigenereCipheringMachine
+	VigenereCipheringMachine
 };
